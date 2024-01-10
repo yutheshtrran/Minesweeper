39169ececbb8c92cd4d0c3453468bde68de2997f
yutheshtrran <153633461+yutheshtrran@users.noreply.github.com>
2024-01-10 12:28:12 +0530

    Update Minesweeper.py
    Display finished

--- Minesweeper.py
+++ Minesweeper.py
@@ -10,7 +10,23 @@ class Minesweeper:
         self.mines = [[False for _ in range(size)] for _ in range(size)]
         self.flags_remaining = mines_count
 
-    def display_field():
+     def display_field(self, reveal_all=False):
+        print("  " + " ".join([chr(65 + i) for i in range(self.grid_size)]))
+        for i in range(self.grid_size):
+            print(chr(65 + i), end=" ")
+            for j in range(self.grid_size):
+                cell_value = self.field[i][j]
+                if reveal_all and self.mines[i][j]:
+                    print('\033[91m' + 'M' + '\033[0m', end=" ")  # Red for revealed mines
+                elif cell_value == 'F':
+                    print('\033[93m' + cell_value + '\033[0m', end=" ")  # Yellow for flags
+                elif cell_value == '.':
+                    print('\033[94m' + cell_value + '\033[0m', end=" ")  # Blue for revealed empty cells
+                elif cell_value.isdigit():
+                    self.print_colored_number(int(cell_value))
+                else:
+                    print('\033[92m' + cell_value + '\033[0m', end=" ")  # Green for covered cells
+            print()
        
 
     def print_colored_number():
